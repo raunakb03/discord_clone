87ae21374bd49f8676a549aafb73204f5d158b1c
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-12-01 01:52:02 +0530

    fixing 404 page not showing bug

--- client/app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ client/app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -12,13 +12,14 @@ const ServerIdLayout = async ({
   params: any;
 }) => {
   const profile = await currentProfile(["members"]);
+  console.log(params.serverId);
   if (!profile) {
     return redirectToSignIn();
   }
   const profileMembers = profile.members;
   try {
     const { data: server } = await axios.get(
-      `${process.env.BASE_URL}/api/server/getServer/${params.serverId[0]}`
+      `${process.env.BASE_URL}/api/server/getServer/${params.serverId}`
     );
     // check if this member is a member of the current server
     let isMember = false;
@@ -37,7 +38,7 @@ const ServerIdLayout = async ({
   return (
     <div className="h-full">
       <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-        <ServerSidebar serverId={params.serverId[0]} />
+        <ServerSidebar serverId={params.serverId} />
       </div>
       <main className="h-full md:pl-60">{children}</main>
     </div>
